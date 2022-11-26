229b20d8bd970fa60f38f4e1b3069fe2d09cd8d4
Gokulakanna <gokulakannamsc@gmail.com>
2022-11-26 23:54:39 +0530

    Initial commit

---
+++ App.java
@@ -0,0 +1,9 @@
+class App
+{
+	public static void main(String[] args)
+	{
+		Calculator calc = new Calculator();
+		System.out.println("---Sum---"+calc.sum(100,50));
+		System.out.println("---Difference---"+calc.difference(100,50));
+	}
+}
---
+++ Calculator.java
@@ -0,0 +1,11 @@
+class Calculator
+{
+	public int sum(int num1,int num2)
+	{
+		return num1+num2;
+	}
+	public int difference(int num1,int num2)
+	{
+		return Math.abs(num1-num2);
+	}
+}
